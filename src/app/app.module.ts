e9b617412b9703b7605708167bc5be7815f67307
xin.gu <xin.gu@accenture.com>
2022-11-03 16:40:52 +0100

    angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { RouterModule, Routes } from '@angular/router';
+import { DemoModule } from './demo/demo.module';
+
+
+const routes:Routes=[
+  {path:'demo', loadChildren:() => import('./demo/demo.module').then(m=>m.DemoModule)},
+  {path:'**', redirectTo:'demo'}
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
