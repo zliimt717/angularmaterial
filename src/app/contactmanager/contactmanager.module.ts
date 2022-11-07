e083c78ff793e2253c2043732d7645829c1bee18
xin.gu <xin.gu@accenture.com>
2022-11-07 16:42:02 +0100

    add conde

--- src/app/contactmanager/contactmanager.module.ts
+++ src/app/contactmanager/contactmanager.module.ts
@@ -5,12 +5,13 @@ import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { MaterialModule } from '../shared/material.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { Routes,RouterModule } from '@angular/router';
 import { UserService } from './services/user.service';
 import {HttpClientModule} from '@angular/common/http';
 import { NotesComponent } from './components/notes/notes.component';
+import { NewContactDialogComponent } from './components/new-contact-dialog/new-contact-dialog.component';
 
 
 const routes:Routes=[
@@ -29,7 +30,8 @@ const routes:Routes=[
     ToolbarComponent,
     MainContentComponent,
     SidenavComponent,
-    NotesComponent
+    NotesComponent,
+    NewContactDialogComponent
   ],
   imports: [
     CommonModule,
@@ -37,6 +39,7 @@ const routes:Routes=[
     MaterialModule,
     FlexLayoutModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild(routes)
   ],
   providers:[
